9cf4dc97975b2b728f6f5730e7e0bc8032d1f44f
cecelialrussell <cecelialrussell@gmail.com>
2025-02-19 07:22:19 -0500

    adding local file

---
+++ variables.py
@@ -0,0 +1,9 @@
+# create a variable called message and print
+
+message = "Hello Python world!"
+print(message)
+
+# create a second variable called mesage and print
+
+message = "Hello Python Crash Course world!"
+print(message)
