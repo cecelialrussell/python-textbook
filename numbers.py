cf65698f8858ab65e3c7bdafc378b9a88fa68b3f
cecelialrussell <cecelialrussell@gmail.com>
2025-02-19 07:59:15 -0500

    initial commit

---
+++ numbers.py
@@ -0,0 +1,13 @@
+# write addition, subtraction, multiplication, and division operations that each result in the number 8
+# print each result
+
+print(4+4)
+print(10-2)
+print(2*4)
+print(16/2)
+
+# use a variable to represent your favorite number
+# create a message that reveals your favorite number
+
+favorite_number = 24
+print(f"My favorite number is {favorite_number}.")
