0f7b4757a69f950b9f872cb0ba714ef8ddccc224
cecelialrussell <cecelialrussell@gmail.com>
2025-02-20 13:28:51 -0500

    created world places file

---
+++ Chapter_3/world_places.py
@@ -0,0 +1,35 @@
+# think of five places in the world and store them in a list
+
+places = ['montana', 'france', 'australia', 'egypt', 'india']
+
+# print the list in its original order
+
+print(places)
+
+# use the sorted function to print list in alphabetical order
+
+print(sorted(places))
+
+# show that you list is still in its original order by printing it
+
+print(places)
+
+# use the reverse function to change the order of the list again
+
+places.reverse()
+print(places)
+
+# use the reverse function again to return list to original order
+
+places.reverse()
+print(places)
+
+# use the sort function to store list in alphabetical order
+
+places.sort()
+print(places)
+
+# use the sort function to store the list in reverse-alphabetical order
+
+places.sort(reverse=True)
+print(places)
