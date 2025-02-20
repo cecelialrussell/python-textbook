0b8998afb1e7d6103fcbf3029ab439636f22ece9
cecelialrussell <cecelialrussell@gmail.com>
2025-02-20 08:27:59 -0500

    initial commit

---
+++ lists.py
@@ -0,0 +1,41 @@
+# store the names of a few of your friends in a list called names.
+# access each element in the list, one at a time.
+
+names = ['bonnie', 'jessica', 'racheal', 'haylee', 'jill']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+
+# print a message to each person in the names list
+
+message = f"Hello, {names[0].title()}. Thank you for being my friend."
+print(message)
+
+message = f"Hello, {names[1].title()}. Thank you for being my friend."
+print(message)
+
+message = f"Hello, {names[2].title()}. Thank you for being my friend."
+print(message)
+
+message = f"Hello, {names[3].title()}. Thank you for being my friend."
+print(message)
+
+message = f"Hello, {names[4].title()}. Thank you for being my friend."
+print(message)
+
+# print a list of places you would like to visit and print a series of statements.
+
+places = ['memphis', 'ireland', 'costa rica', 'yellowstone']
+message = f"I would like to visit {places[0].title()}."
+print(message)
+
+message = f"I would like to visit {places[1].title()}."
+print(message)
+
+message = f"I would like to visit {places[2].title()}."
+print(message)
+
+message = f"I would like to visit {places[3].title()}."
+print(message)
