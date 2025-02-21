b3724c8895366e078f22d5277887c45b277f2d50
cecelialrussell <cecelialrussell@gmail.com>
2025-02-21 08:42:39 -0500

    adding pizzas exercise to ch. 4

---
+++ Chapter_4/pizzas.py
@@ -0,0 +1,18 @@
+# create a list with three of your favorite kinds of pizza
+# use a for loop to print the name of each pizza
+
+pizzas = ['sausage', 'pepperoni', 'chicken alfredo']
+for pizza in pizzas:
+    print(pizza)
+
+# modify the for loop to print a sentence with the name of each pizza
+
+for pizza in pizzas:
+    print(f"I would like to order a {pizza} pizza, please.")
+
+# add a closing statement outside the for loop
+
+for pizza in pizzas:
+    print(f"I would like to order a {pizza} pizza, please.")
+
+print(f"I really love pizza. It's my favorite!")
