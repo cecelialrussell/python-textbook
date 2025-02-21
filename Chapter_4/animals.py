18419025e2cc66334ac5e3b54a1a6c005c3c6d84
cecelialrussell <cecelialrussell@gmail.com>
2025-02-21 08:52:09 -0500

    added animals exercise to ch. 4

---
+++ Chapter_4/animals.py
@@ -0,0 +1,17 @@
+# add three animals to a list that have something in common and use a for loop to print the name of each
+
+animals = ['otter', 'ferret', 'mink']
+for animal in animals:
+    print(animal)
+
+# modify the for loop to print a statement about each animal
+
+for animal in animals:
+    print(f"The {animal} is part of the Mustelidae family.")
+
+# add a closing statement outside of the for loop
+
+for animal in animals:
+    print(f"The {animal} is part of the Mustelidae family.")
+
+print(f"Only the ferret has been domesticated.")
