b4fc57735d77986e613b9a3e4e3dceef94a2285e
cecelialrussell <cecelialrussell@gmail.com>
2025-03-01 10:11:14 -0500

    adding ch. 5 exercise

---
+++ Chapter_5/voting.py
@@ -0,0 +1,16 @@
+# create a variable for age and write an if statement to determine if person is old enough to vote
+
+age = 19
+if age >= 18:
+    print(f"You are old enough to vote!")
+    print(f"Have you registered to vote yet?")
+
+# add an if else statement for those who are not old enough to vote
+
+age = 17
+if age >= 18:
+    print(f"You are old enough to vote!")
+    print(f"Have you registered to vote yet?")
+else:
+    print(f"Sorry, you are too young to vote.")
+    print(f"Please register to vote as soon as you turn 18.")
