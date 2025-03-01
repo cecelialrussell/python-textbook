8f5434fce035ef76b4e0b2670b826e875e698f01
cecelialrussell <cecelialrussell@gmail.com>
2025-03-01 10:29:49 -0500

    updating toppings exercise

--- Chapter_5/toppings.py
+++ Chapter_5/toppings.py
@@ -5,3 +5,16 @@ requested_topping = 'mushrooms'
 
 if requested_topping != 'anchovies':
     print(f"Hold the anchovies!")
+
+# write multiple if statements to provide instructions for all requested toppings
+
+requested_toppings = ['mushrooms', 'extra cheese']
+
+if 'mushrooms' in requested_toppings:
+    print(f"Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print(f"Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print(f"Adding extra cheese.")
+
+print(f"Finished making your pizza.")
