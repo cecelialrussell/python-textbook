dab47a518305ad5115fa4045903d7932bd356a6e
cecelialrussell <cecelialrussell@gmail.com>
2025-03-01 11:07:11 -0500

    adding ch. 5 exercise

---
+++ Chapter_5/alien_colors.py
@@ -0,0 +1,48 @@
+# create a variable called alien_color and assign a value of green, yellow, or red
+# write an if statement to determine if the alien's color is green
+# if True, print a message that the player gets 5 points
+
+alien_color = 'green'
+if alien_color == 'green':
+    print(f"You just earned 5 points!")
+
+# write a version of the program where the alien is not green
+
+alien_color = 'red'
+if alien_color == 'green':
+    print(f"You just earned 5 points!")
+
+# add an else statement to the program to give the player 10 points if the alien is not green
+
+alien_color = 'red'
+if alien_color == 'green':
+    print(f"You just earned 5 points!")
+else:
+    print(f"You just earned 10 points!")
+
+# convert the if-else program into an if-elif-else chain
+# give 5 points in the alien is green, 10 points if the alien is yellow, and 15 points if the alien is red
+
+alien_color = 'green'
+if alien_color == 'green':
+    print(f"You just earned 5 points!")
+elif alien_color == 'yellow':
+    print(f"You just earned 10 points!")
+else:
+    print(f"You just earned 15 points!")
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print(f"You just earned 5 points!")
+elif alien_color == 'yellow':
+    print(f"You just earned 10 points!")
+else:
+    print(f"You just earned 15 points!")
+
+alien_color = 'red'
+if alien_color == 'green':
+    print(f"You just earned 5 points!")
+elif alien_color == 'yellow':
+    print(f"You just earned 10 points!")
+else:
+    print(f"You just earned 15 points!")
