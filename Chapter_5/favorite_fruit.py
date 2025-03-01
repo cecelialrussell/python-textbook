c727e6d516f9fb738a827b0fe1b0d938879dcb71
cecelialrussell <cecelialrussell@gmail.com>
2025-03-01 11:17:31 -0500

    adding ch. 5 exercise

---
+++ Chapter_5/favorite_fruit.py
@@ -0,0 +1,13 @@
+# write a list of favorite fruits
+# write a series of if statements to check for certain fruits in the list
+
+favorite_fruits = ['apples', 'grapes', 'strawberries']
+
+if 'strawberries' in favorite_fruits:
+    print(f"You really like strawberries!")
+
+if 'apples' in favorite_fruits:
+    print(f"You really like apples!")
+
+if 'bananas' in favorite_fruits:
+    print(f"You really like bananas!")
