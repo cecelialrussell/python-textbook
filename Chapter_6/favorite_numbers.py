3e4af4bb2ae45b1c706c4d4198d9f03df55710ae
cecelialrussell <cecelialrussell@gmail.com>
2025-10-28 09:04:05 -0400

    updating chapter 6

---
+++ Chapter_6/favorite_numbers.py
@@ -0,0 +1,13 @@
+favorite_numbers = {
+    'lucy': 21,
+    'steve': 7,
+    'amanda': 11,
+    'rick': 16,
+    'bobbie': 24,
+}
+
+print(f"Lucy's favorite number is {favorite_numbers['lucy']}.")
+print(f"Steve's favorite number is {favorite_numbers['steve']}.")
+print(f"Amanda's favorite number is {favorite_numbers['amanda']}.")
+print(f"Rick's favorite number is {favorite_numbers['rick']}.")
+print(f"Bobbie's favorite number is {favorite_numbers['bobbie']}.")
