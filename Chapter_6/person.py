809580a04127f4e0d8795ad0b7bf47d022251886
cecelialrussell <cecelialrussell@gmail.com>
2025-10-28 08:59:19 -0400

    updating chapter 6

---
+++ Chapter_6/person.py
@@ -0,0 +1,8 @@
+person = {
+    'first_name': 'jessica',
+    'last_name': 'swander',
+    'age': 43,
+    'city': 'fremont',
+}
+
+print(f"The person's name is {person['first_name'].title()} {person['last_name'].title()}. She is {person['age']} years old and lives in {person['city'].title()}.")
