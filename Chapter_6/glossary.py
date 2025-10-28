4ec336177a04f1a0d9d6931c91dc772d5500ac2e
cecelialrussell <cecelialrussell@gmail.com>
2025-10-28 09:17:30 -0400

    updating chapter 6

---
+++ Chapter_6/glossary.py
@@ -0,0 +1,17 @@
+glossary = {
+    'variable': 'a container for storing a data value',
+    'string': 'an ordered collection of characters that can include letters, numbers, symbols, and spaces',
+    'integer': 'a fundamental data type used to represent whole numbers',
+    'float': 'a fundamental data type used to represent real numbers that can have decimal points',
+    'tuple': 'an ordered, immutable collection of elements that is similar to a list but cannot be modified once created',
+    'list': 'a fundamental, built-in data structure used to store an ordered collection of items',
+    'dictionary': 'a built-in data type that represents an unordered collection of key-value pairs',   
+}
+
+print(f"A variable is {glossary['variable']}.")
+print(f"A string is {glossary['string']}.")
+print(f"An integer is {glossary['integer']}.")
+print(f"A float is {glossary['float']}.")
+print(f"A tuple is {glossary['tuple']}.")
+print(f"A list is {glossary['list']}.")
+print(f"A dictionary is {glossary['dictionary']}.")
